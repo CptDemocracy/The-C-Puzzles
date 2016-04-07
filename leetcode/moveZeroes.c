ae0be6b76c2cf5cf153c399e5abe2898eaa0d1ed
Efim Bogdanovsky <profit234@gmail.com>
2016-04-07 21:50:22 +0300

    Create moveZeroes.c

---
+++ leetcode/moveZeroes.c
@@ -0,0 +1,36 @@
+/*
+[ref.href] https://leetcode.com/problems/move-zeroes
+
+"
+  Given an array nums, write a function to move all 0's to the end of it while maintaining 
+  the relative order of the non-zero elements.
+  
+  For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be 
+  [1, 3, 12, 0, 0].
+  
+  Note:
+  - You must do this in-place without making a copy of the array.
+  - Minimize the total number of operations.
+  
+  Credits:
+  Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
+"
+*/
+
+void moveZeroes(int* nums, int numsSize) {
+	if (numsSize < 1) return;
+	int i = 0;
+	int j = 0;
+	int nonzcount = 0;
+	while (j < numsSize) {
+		if (nums[j] != 0) {
+			nums[i] = nums[j];
+			++i, ++nonzcount;
+		}
+		++j;
+	}
+	while (nonzcount < numsSize) {
+		nums[nonzcount] = 0;
+		++nonzcount;
+	}
+}
