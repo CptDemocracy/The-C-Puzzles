73ed0f0217e1f7089300e4807780327c5ee54659
Efim Bogdanovsky <profit234@gmail.com>
2016-04-10 20:04:29 +0300

    Update plusOne.c

--- leetcode/April-10th-2016/plusOne.c
+++ leetcode/April-10th-2016/plusOne.c
@@ -14,33 +14,33 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* plusOne(int* digits, int digitsSize, int* returnSize) {
-	int* result = (int*)calloc(digitsSize, sizeof(int));
-	if (!result) return NULL;
+    int* result = (int*)calloc(digitsSize, sizeof(int));
+    if (!result) return NULL;
 
-	int sum = digits[digitsSize - 1] + 1;
-	result[digitsSize - 1] = sum % 10;
-	int carry = sum / 10;
+    int sum = digits[digitsSize - 1] + 1;
+    result[digitsSize - 1] = sum % 10;
+    int carry = sum / 10;
 
-	for (int i = digitsSize - 2; i >= 0; --i) {
-		sum = digits[i] + carry;
-		carry = sum / 10;
-		result[i] = sum % 10;
-	}
-	if (carry > 0) {
-		int* newResult = (int*)calloc(digitsSize + 1, sizeof(int));
-		if (!newResult) {
-			free(result);
-			return NULL;
-		}
-		memcpy(&newResult[1], result, digitsSize * sizeof(int));
-		free(result);
-		result = newResult;
-		result[0] = carry;
-		*returnSize = digitsSize + 1;
-	}
-	else {
-		*returnSize = digitsSize;
-	}
+    for (int i = digitsSize - 2; i >= 0; --i) {
+        sum = digits[i] + carry;
+        carry = sum / 10;
+        result[i] = sum % 10;
+    }
+    if (carry > 0) {
+        int* newResult = (int*)calloc(digitsSize + 1, sizeof(int));
+        if (!newResult) {
+            free(result);
+            return NULL;
+        }
+        memcpy(&newResult[1], result, digitsSize * sizeof(int));
+        free(result);
+        result = newResult;
+        result[0] = carry;
+        *returnSize = digitsSize + 1;
+    }
+    else {
+        *returnSize = digitsSize;
+    }
 
-	return result;
+    return result;
 }
