deec495ff070199b53cdeeab3c6647ddc4178ca1
Efim Bogdanovsky <profit234@gmail.com>
2016-04-15 13:23:52 +0300

    Create pow.c

---
+++ leetcode/pow.c
@@ -0,0 +1,40 @@
+/*
+[ref.href] leetcode.com/problems/powx-n
+"
+  Implement pow(x, n).
+"
+*/
+
+#include <math.h>
+#define EPSILON (1e-6)
+
+double myPow(double x, int n) {
+    if (n == 0) {
+        return 1;
+    }
+    if (n == -n) {
+        // guard against power==INT_MIN shenanigans
+        x = abs(x);
+        if (abs(x - 1) <= EPSILON) {
+            return x;
+        }
+        return 0.0;
+    }
+    if (n < 0) {
+        x = 1.0 / x;
+        n = -n;
+    }
+    double y = 1.0;
+    while (n > 1) {
+        if (n % 2 == 0) {
+            x *= x;
+            n >>= 1;
+        }
+        else {
+            y *= x;
+            x *= x;
+            n = (n - 1) >> 1;
+        }
+    }
+    return x * y;
+}
