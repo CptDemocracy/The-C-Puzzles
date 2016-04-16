dee06d08f582b485a5decf5c1127e0ae465bab25
Efim Bogdanovsky <profit234@gmail.com>
2016-04-16 12:31:25 +0300

    Update rotateArray.c

--- leetcode/April-16th-2016/rotateArray.c
+++ leetcode/April-16th-2016/rotateArray.c
@@ -1,3 +1,15 @@
+/*
+[ref.href] leetcode.com/problems/rotate-array
+"
+    Rotate an array of n elements to the right by k steps.
+
+    For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+    
+    Credits:
+    Special thanks to @Freezen for adding this problem and creating all test cases.
+"
+*/
+
 void reverse(int* nums, int start, int stop) {
     if (!nums) return;
     
